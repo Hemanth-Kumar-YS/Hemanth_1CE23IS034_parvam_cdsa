3a61636549280b44dc3278d1bc0f0e0ad0ac2923
Hemanth-Kumar-YS <hemanthkumarys07@gmail.com>
2024-11-08 15:35:19 +0530

    Add files via upload

---
+++ a7.c
@@ -0,0 +1,14 @@
+include<stdio.h>
+#include<string.h>
+
+int isPallin(char*);
+int main()
+{
+    printf("Hello World");
+    return 0;
+}
+int isPallin(char *s )
+{
+    static int i=0,len=strlen(s);
+    if(s)
+}
